282065c1144eb6d369ec3a8e76071f1abac8f684
Duncan Calvert <dcalvert@ihmc.us>
2021-05-15 15:48:09 -0500

    Implement the rest of the enums.

--- imgui-binding/src/main/java/imgui/extension/implot/flag/ImPlotOrientation.java
+++ imgui-binding/src/main/java/imgui/extension/implot/flag/ImPlotOrientation.java
@@ -1,4 +1,11 @@
 package imgui.extension.implot.flag;
 
 public class ImPlotOrientation {
+    private ImPlotOrientation() {
+    }
+
+    /** left/right */
+    public static final int Horizontal = 0;
+    /** up/down */
+    public static final int Vertical   = 1;
 }
--- imgui-binding/src/main/java/imgui/extension/implot/flag/ImPlotYAxis.java
+++ imgui-binding/src/main/java/imgui/extension/implot/flag/ImPlotYAxis.java
@@ -1,4 +1,13 @@
 package imgui.extension.implot.flag;
 
 public class ImPlotYAxis {
+    private ImPlotYAxis() {
+    }
+
+    /** left (default) */
+    public static final int YAxis_1 = 0;
+    /** first on right side */
+    public static final int YAxis_2 = 1;
+    /** second on right side */
+    public static final int YAxis_3 = 2;
 }
