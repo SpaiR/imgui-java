acc4e06e39b2b80cc20f10901fb5068249dadaf8
Perry MacMurray <perrymacmurray@gmail.com>
2021-06-14 15:47:01 -0500

    Implement ImPlotContext and related methods

--- imgui-binding/src/main/java/imgui/extension/implot/ImPlot.java
+++ imgui-binding/src/main/java/imgui/extension/implot/ImPlot.java
@@ -3,6 +3,12 @@ package imgui.extension.implot;
 import imgui.type.ImBoolean;
 
 public final class ImPlot {
+    private static final ImPlotContext IMPLOT_CONTEXT;
+
+    static {
+        IMPLOT_CONTEXT = new ImPlotContext(0);
+    }
+
     private ImPlot() {
 
     }
@@ -13,8 +19,50 @@ public final class ImPlot {
 
      */
 
-    public static native void createContext(); /*
-        ImPlot::CreateContext();
+    /**
+     * Creates a new ImPlot context. Call this after ImGui.createContext().
+     */
+    public static ImPlotContext createContext() {
+        IMPLOT_CONTEXT.ptr = nCreateContext();
+        return IMPLOT_CONTEXT;
+    }
+
+    private static native long nCreateContext(); /*
+        return (intptr_t)ImPlot::CreateContext();
+    */
+
+    /**
+     * Destroys an ImPlot context. Call this before ImGui.destroyContext(). NULL = destroy current context.
+     */
+    public static void destroyContext(final ImPlotContext ctx) {
+        nDestroyContext(ctx.ptr);
+    }
+
+    private static native void nDestroyContext(long ctx); /*
+        ImPlot::DestroyContext((ImPlotContext*)ctx);
+    */
+
+    /**
+     * Returns the current ImPlot context. NULL if no context has ben set.
+     */
+    public static ImPlotContext getCurrentContext() {
+        IMPLOT_CONTEXT.ptr = nGetCurrentContext();
+        return IMPLOT_CONTEXT;
+    }
+
+    private static native long nGetCurrentContext(); /*
+        return (intptr_t)ImPlot::GetCurrentContext();
+    */
+
+    /**
+     * Sets the current ImPlot context.
+     */
+    public static void setCurrentContext(final ImPlotContext ctx) {
+        nSetCurrentContext(ctx.ptr);
+    }
+
+    private static native void nSetCurrentContext(long ctx); /*
+        ImPlot::SetCurrentContext((ImPlotContext*)ctx);
     */
 
     public static void showDemoWindow(final ImBoolean pOpen) {
---
+++ imgui-binding/src/main/java/imgui/extension/implot/ImPlotContext.java
@@ -0,0 +1,11 @@
+package imgui.extension.implot;
+
+import imgui.binding.ImGuiStruct;
+
+public final class ImPlotContext extends ImGuiStruct
+{
+    public ImPlotContext(final long ptr)
+    {
+        super(ptr);
+    }
+}
