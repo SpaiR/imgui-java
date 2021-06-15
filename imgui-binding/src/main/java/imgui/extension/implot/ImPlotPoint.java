aff9de2c21367967d3e56e942679031c3d848e73
Perry MacMurray <perrymacmurray@gmail.com>
2021-06-15 10:36:11 -0500

    Implement ImPlotPoint

--- imgui-binding/src/main/java/imgui/extension/implot/ImPlotPoint.java
+++ imgui-binding/src/main/java/imgui/extension/implot/ImPlotPoint.java
@@ -1,4 +1,66 @@
 package imgui.extension.implot;
 
-public final class ImPlotPoint {
+import imgui.ImVec2;
+import imgui.binding.ImGuiStruct;
+
+public final class ImPlotPoint extends ImGuiStruct
+{
+    public ImPlotPoint(long ptr)
+    {
+        super(ptr);
+    }
+
+    public ImPlotPoint() {
+        this(0, 0);
+    }
+
+    public ImPlotPoint(ImVec2 vec) {
+        this(vec.x, vec.y);
+    }
+
+    public ImPlotPoint(double x, double y) {
+        this(create(x, y));
+    }
+
+    /*JNI
+        #include "_common.h"
+        #include "_implot.h"
+
+     */
+
+    private static native long create(double x, double y); /*
+        return (intptr_t)(new ImPlotPoint(x, y));
+    */
+
+    public double getX() {
+        return nGetX(this.ptr);
+    }
+
+    private static native double nGetX(long ptr); /*
+        return ((ImPlotPoint*)ptr)->x;
+    */
+
+    public double getY() {
+        return nGetY(this.ptr);
+    }
+
+    private native double nGetY(long ptr); /*
+        return ((ImPlotPoint*)ptr)->y;
+     */
+
+    public void setX(double value) {
+        nSetX(this.ptr, value);
+    }
+
+    private static native void nSetX(long ptr, double value); /*
+        ((ImPlotPoint*)ptr)->x = value;
+     */
+
+    public void setY(double value) {
+        nSetY(this.ptr, value);
+    }
+
+    private static native void nSetY(long ptr, double value); /*
+        ((ImPlotPoint*)ptr)->y = value;
+     */
 }
