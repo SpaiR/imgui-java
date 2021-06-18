72c9ca5ed091677541d95b02fa6ac976533c7488
Perry <pmacmurray@ihmc.org>
2021-06-18 12:24:39 -0500

    Fix ImPlot example program bug

--- example/src/main/java/ExampleImPlot.java
+++ example/src/main/java/ExampleImPlot.java
@@ -12,22 +12,25 @@ public class ExampleImPlot {
     public static void show(ImBoolean showImPlotWindow) {
         ImGui.setNextWindowSize(500, 400, ImGuiCond.Once);
         ImGui.setNextWindowPos(ImGui.getMainViewport().getPosX() + 100, ImGui.getMainViewport().getPosY() + 100, ImGuiCond.Once);
-        if (ImGui.begin("ImPlot Demo", showImPlotWindow)) {
+        if (ImGui.begin("ImPlot Demo", showImPlotWindow))
+        {
             ImGui.text("This a demo for ImPlot");
 
             final Integer[] xs = {0, 1, 2, 3, 4, 5};
             final Integer[] ys = {0, 1, 2, 3, 4, 5};
 
-            ImPlot.beginPlot("Example Plot");
+            if (ImPlot.beginPlot("Example Plot")) {
 
             ImPlot.plotLine("Line", xs, ys);
             ImPlot.plotBars("Bars", xs, ys);
 
             ImPlot.endPlot();
+            }
 
-            ImPlot.beginPlot("Example Scatterplot");
-            ImPlot.plotScatter("Scatter", xs, ys);
-            ImPlot.endPlot();
+            if (ImPlot.beginPlot("Example Scatterplot")) {
+                ImPlot.plotScatter("Scatter", xs, ys);
+                ImPlot.endPlot();
+            }
         }
 
         ImGui.end();
