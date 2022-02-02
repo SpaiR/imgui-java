a6ca631506234ebd318fb569b4188b3421f0e111
SpaiR <klimbetoo@gmail.com>
2022-02-02 20:45:09 +0200

    [Build] Add more logs to the release script

--- buildSrc/scripts/release.sh
+++ buildSrc/scripts/release.sh
@@ -3,15 +3,24 @@
 BASEDIR=$(dirname "$0")
 cd $BASEDIR/../.. || exit
 
-echo [Publishing Modules...]
+echo > Publishing Modules...
+echo >> Module [imgui-app]
 ./gradlew :imgui-app:publishMavenJavaPublicationToMavenCentralRepository
+echo >> Module [imgui-lwjgl3]
 ./gradlew :imgui-lwjgl3:publishMavenJavaPublicationToMavenCentralRepository
+echo >> Module [imgui-binding]
 ./gradlew :imgui-binding:publishMavenJavaPublicationToMavenCentralRepository
 
-echo [Publishing Native...]
+echo > Publishing Natives...
+echo >> Natives: [win & freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=win -Pfreetype=true
+echo >> Natives: [win & no freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=win -Pfreetype=false
+echo >> Natives: [linux & freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=linux -Pfreetype=true
+echo >> Natives: [linux & no freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=linux -Pfreetype=false
+echo >> Natives: [mac & freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=mac -Pfreetype=true
+echo >> Natives: [mac & no freetype]
 ./gradlew :imgui-binding-natives:publishMavenJavaPublicationToMavenCentralRepository -PdeployType=mac -Pfreetype=false
