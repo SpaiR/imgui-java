f3f31b52ac780c29f89abd76b36179b8ee4bbd3d
SpaiR <klimbetoo@gmail.com>
2022-12-06 10:07:08 +0200

    [Build] Fix GenerateLibs

--- buildSrc/scripts/build-ft-local.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname "$0")
-cd $BASEDIR/../../imgui-binding || exit
-
-../gradlew clean generateLibs -Denvs=$* -Dlocal -Dfreetype=true
-rm -frd libsNative
--- buildSrc/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname "$0")
-cd $BASEDIR/../../imgui-binding || exit
-
-rm -frd /tmp/imgui
-../gradlew clean generateLibs -Denvs=$*
