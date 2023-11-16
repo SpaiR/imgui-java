2ddac4e05d9c53b5c861c9c066f1c0c2773a70b2
SpaiR <klimbetoo@gmail.com>
2023-11-16 09:03:05 +0200

    Fix generation for methods with ARRAY and POINTER flags in args

--- buildSrc/src/main/kotlin/tool/generator/api/definition/node/transform/method/add method for pointer with array.kt
+++ buildSrc/src/main/kotlin/tool/generator/api/definition/node/transform/method/add method for pointer with array.kt
@@ -3,13 +3,12 @@ package tool.generator.api.definition.node.transform.method
 import tool.generator.api.definition.node.DefinitionNode
 import tool.generator.api.definition.node.Nodes
 import tool.generator.api.definition.node.transform.TransformationChain
-import tool.generator.api.definition.node.type.method.ArgsDefinitionNode
 import tool.generator.api.definition.node.type.method.MethodDefinitionNode
 import tool.generator.api.definition.node.type.method.ext.*
 
 /**
  * Transformation creates additional JVM method,
- * when one or multiple arguments have [ArgTypeFlag.POINTER_WITH_ARRAY] flag.
+ * when one or multiple arguments have [ArgTypeFlag.ARRAY] and [ArgTypeFlag.POINTER] flags.
  * Created method will be the same, but instead of args with flag it will have appropriate arrays.
  */
 @Suppress("ClassName")
@@ -18,39 +17,40 @@ object `add method for pointer with array` : TransformationChain.Transform {
         val result = mutableListOf<DefinitionNode>()
 
         nodes.forEach { node ->
-            result += node
-
             if (node is MethodDefinitionNode && !node.signature.isNative) {
-                val args = node.signature.argsList
-                var newMethod: MethodDefinitionNode? = null
+                // We make a copy of the initial node to avoid unwanted modifications.
+                val initialNode = node.copy()
+                val initialArgs = initialNode.signature.args.args
 
-                args.forEachIndexed { index, arg ->
+                val newMethod = node.copy()
+                val newArgs = newMethod.signature.argsList.toMutableList()
+
+                var hasNewMethodToAdd = false
+                initialArgs.forEachIndexed { index, arg ->
                     if (arg.argType.hasFlag(ArgTypeFlag.POINTER) && arg.argType.hasFlag(ArgTypeFlag.ARRAY)) {
-                        if (newMethod == null) {
-                            newMethod = node.copy()
-                        }
+                        hasNewMethodToAdd = true
 
-                        // Remove ARRAY flag so the method will be generated with a pointer arg.
+                        // Remove ARRAY flag so the method will be generated with a pointer arg only.
                         arg.argType.removeFlag(ArgTypeFlag.ARRAY)
 
-                        newMethod!!.let {
-                            val newArgs = it.signature.argsList.toMutableList()
-                            val newArg = newArgs[index]
-                            newArgs[index] = newArg
-
-                            // New method will be generated with an argument as an array.
-                            newArg.argType.removeFlag(ArgTypeFlag.POINTER)
-
-                            it.signature.args = ArgsDefinitionNode().apply {
-                                this.args = newArgs
-                            }
-                        }
+                        // New method will be generated with an argument as an array.
+                        val newArg = newArgs[index]
+                        newArgs[index] = newArg
+                        newArg.argType.removeFlag(ArgTypeFlag.POINTER)
                     }
                 }
 
-                if (newMethod != null) {
-                    result += newMethod!!
+                newMethod.signature.args.args = newArgs
+
+                // Add the initial node to the result list.
+                result += initialNode
+
+                // If we have args where we need to remove ARRAY flag - there is a method to be added.
+                if (hasNewMethodToAdd) {
+                    result += newMethod
                 }
+            } else {
+                result += node
             }
         }
 
