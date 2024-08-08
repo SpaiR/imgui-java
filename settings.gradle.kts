1c396fb82a77715ebe44a8b60471203376f94611
SpaiR <klimbetoo@gmail.com>
2024-08-08 09:09:41 +0300

    build(ci): adapt build pipeline for the generator API
    build(ci): make "Compare Binding Hash" job also took into account generated and include folders
    
    build(ci): add JDK 11 to the setup-java pipeline
    
    This is required for the buildSrc module, since it uses "spoon" library, which is compiled for JDK11.
    
    build(ci): keep only JDK11
    
    - Add source ant target compatibility output to the MANIFEST.mf file
    
    build: add auto-provisioning for JDK
    
    just in case

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.8.0")
+}
+
+rootProject.name = "imgui-java"
+include("imgui-binding")
+include("imgui-lwjgl3")
+include("imgui-binding-natives")
+include("imgui-app")
+include("example")
